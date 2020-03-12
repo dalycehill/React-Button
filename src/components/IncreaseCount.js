6eb09ad16ecf14622d3acc8690cbf3525eed4886
Dalyce Hill <50115499+dalycehill@users.noreply.github.com>
2020-03-12 09:27:44 -0300

    Add files via upload

---
+++ src/components/IncreaseCount.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import Button from './Button'
+import './IncreaseCount.css';
+
+class IncreaseCount extends React.Component {
+
+    _increment = () => {
+        this.props.onIncrement(this.state);
+    };
+
+    render() {
+        return(
+            <div>
+                <Button className="plus" title="+" onClick={this._increment}/>
+            </div>
+        );
+    }
+}
+
+export default IncreaseCount;
